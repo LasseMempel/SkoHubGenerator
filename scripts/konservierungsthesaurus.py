5879fe8f4eb11a8b8fe4666e4d881c9e00dc850c
Lasse Mempel-Länger <lassemempel@gmail.com>
2025-12-11 09:17:13 +0100

    refactoring

--- scripts/konservierungsthesaurus.py
+++ scripts/konservierungsthesaurus.py
@@ -1,65 +1,63 @@
 import requests
 import pandas as pd
 from rdflib import Graph, URIRef, Literal
-from rdflib.namespace import SKOS, RDF, DC, DCTERMS, RDFS, VANN
+from rdflib.namespace import SKOS, RDF, DCTERMS, RDFS, VANN
 
 def csv2Df(link, filename):
+
     with open(filename, "w", encoding="utf-8") as f:
         f.write(requests.get(link).text.encode("ISO-8859-1").decode())
     df = pd.read_csv(filename, encoding="utf-8")
-
-    # fix in table pls!
-    # fix to replace linebreaks with pipeseperators for mapping properties, which don't follow the seperator rules
-    for col in ["closeMatch", "relatedMatch", "exactMatch"]:
-        if col in df.columns:
-            df[col] = df[col].map(lambda x: "|".join(x.split("\n")) if isinstance(x, str) else x)
     
     return df
 
+def row2Triple(value, g, subj, pred, obj, isLang, namespace, scheme, lang):
 
-"""
-def useSemanticAatUris(df):
-    for index, row in df.iterrows():
-        if row["prefLabel"] and isinstance(row["prefLabel"], str) and row["notation"] and isinstance(row["notation"], str):
-            # in columns "closeMatch" and "relatedMatch" replace "vocab.getty.edu/page/aat/" with vocab.getty.edu/aat/
-            oldRelatedMatch = row["relatedMatch"]
-            oldCloseMatch = row["closeMatch"]
-            if oldRelatedMatch and isinstance(oldRelatedMatch, str):
-                df.at[index, "relatedMatch"] = oldRelatedMatch.replace("vocab.getty.edu/page/aat/", "vocab.getty.edu/aat/")
-            if oldCloseMatch and isinstance(oldCloseMatch, str):
-                df.at[index, "closeMatch"] = oldCloseMatch.replace("vocab.getty.edu/page/aat/", "vocab.getty.edu/aat/")
-    return df
-"""
-
-def row2Triple(i, g, subj, pred, obj, isLang, namespace, scheme):
-    i = i.strip()
-    if i == "":
+    value = value.strip()
+    if value == "":
         print("Empty cell")
         print(subj, pred, obj)
         return g
     if obj == URIRef:
         if pred in [SKOS.broader, SKOS.narrower, SKOS.related]:
-            if i != "top":
-                g.add ((subj, pred, URIRef(namespace + i)))
+            if value != "top":
+                g.add ((subj, pred, URIRef(namespace + value)))
                 if pred == SKOS.broader:
-                    g.add ((URIRef(namespace + i), SKOS.narrower, subj))
+                    g.add ((URIRef(namespace + value), SKOS.narrower, subj))
             else:
                 g.add ((subj, SKOS.topConceptOf, scheme))
         else:
-            g.add ((subj, pred, URIRef(i))) #urllib.parse.quote(i)
+            g.add ((subj, pred, URIRef(value)))
     else:
         if isLang:
-            if len(i) > 2 and i[-3] == "@":
-                i, baseLanguageLabel = i.split("@")
-            g.add ((subj, pred, obj(i, lang= baseLanguageLabel)))
+            g.add ((subj, pred, obj(value, lang= lang)))
         else:
-            g.add ((subj, pred, obj(i)))
+            g.add ((subj, pred, obj(value)))
+    return g
+
+def propertyWalk(df, row, g, subj, scheme, namespace):
+
+    #for prop, pred, obj, isLang in propertyDict:
+    for col in df.columns:
+        if "@" in col:
+            colProp, lang = col.split("@")
+        else:
+            colProp, lang = col, baseLanguageLabel
+        
+        if colProp in propertyDict:
+            pred, obj, isLang = propertyDict[colProp]
+            if not isinstance(row[col], float):
+                if seperator in row[col]:
+                    seperatedValues = row[col].split(seperator)
+                else:
+                    seperatedValues = [row[col]]
+                for value in seperatedValues:
+                    g = row2Triple(value, g, subj, pred, obj, isLang, namespace, scheme, lang)
     return g
 
 def df2Skos(schemeDf, conceptsDf):
 
     g = Graph()
-
     # extract and declare conceptScheme and namespace
     for index, row in schemeDf.iterrows():
         if row["ConceptScheme"] and isinstance(row["ConceptScheme"], str) and row["namespace"] and isinstance(row["namespace"], str):
@@ -67,36 +65,16 @@ def df2Skos(schemeDf, conceptsDf):
             g.add ((scheme, RDF.type, SKOS.ConceptScheme))
             namespace = row["namespace"]
             g.add((scheme, VANN.preferredNamespaceUri, Literal(namespace)))
-
-    # declare conceptScheme metadata
-    for prop, pred, obj, isLang in propertyTuples:
-        if prop in schemeDf.columns:
-            if not isinstance(row[prop], float):
-                if seperator in row[prop]:
-                    seperatedValues = row[prop].split(seperator)
-                else:
-                    seperatedValues = [row[prop]]
-                for value in seperatedValues:
-                    g = row2Triple(value, g, scheme, pred, obj, isLang, namespace, scheme)
+            g = propertyWalk(schemeDf, row, g, scheme, scheme, namespace)
 
     # declare concepts
     for index, row in conceptsDf.iterrows():
         # check if prefLabel and notation have a non empty string value
-        if row["prefLabel"] and isinstance(row["prefLabel"], str) and row["notation"] and isinstance(row["notation"], str):
-            #print(row["prefLabel"], row["notation"])
+        if row["prefLabel"+"@" + baseLanguageLabel] and isinstance(row["prefLabel"+"@" + baseLanguageLabel], str) and row["notation"] and isinstance(row["notation"], str):
             concept = URIRef(namespace + row['notation'])
             g.add ((concept, RDF.type, SKOS.Concept))
-            for prop, pred, obj, isLang in propertyTuples:
-                if prop in df.columns:
-                    if not isinstance(row[prop], float):
-                        if seperator in row[prop]:
-                            seperated = row[prop].split(seperator)
-                            langs = [x.split("@") for x in seperated]
-                            for i in range(len(seperated)):
-                                g = row2Triple(seperated[i], g, concept, pred, obj, isLang, baseLanguageLabel, namespace, scheme)
-                        else:
-                            g = row2Triple(row[prop], g, concept, pred, obj, isLang, baseLanguageLabel, namespace, scheme)
             g.add ((concept, SKOS.inScheme, scheme))
+            g = propertyWalk(conceptsDf, row, g, concept, scheme, namespace)
             if row["broader"] == "top":
                 g.add ((scheme, SKOS.hasTopConcept, concept))
                 g.add ((concept, SKOS.topConceptOf, scheme))
@@ -112,52 +90,52 @@ conceptsLink = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSJV7qC1QYCAYghp
 schemeLink = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSJV7qC1QYCAYghp8SX09EatvnXPurJ9ZMAsGE1iUrPIxL4nLiyXlYBtKBi1Zf1xTG10AXzUp3pZcxx/pub?gid=157607640&single=true&output=csv"
 baseLanguageLabel = "de"
 
-propertyTuples = [
+propertyDict = {
     # SKOS Mapping Properties
-    ("broadMatch", SKOS.broadMatch, URIRef, False),
-    ("narrowMatch", SKOS.narrowMatch, URIRef, False),
-    ("relatedMatch", SKOS.relatedMatch, URIRef, False),
-    ("closeMatch", SKOS.closeMatch, URIRef, False),
-    ("exactMatch", SKOS.exactMatch, URIRef, False),
+    "broadMatch": (SKOS.broadMatch, URIRef, False),
+    "narrowMatch": (SKOS.narrowMatch, URIRef, False),
+    "relatedMatch": (SKOS.relatedMatch, URIRef, False),
+    "closeMatch": (SKOS.closeMatch, URIRef, False),
+    "exactMatch": (SKOS.exactMatch, URIRef, False),
     
     # SKOS Semantic Relations
-    ("broader", SKOS.broader, URIRef, False),
-    ("narrower", SKOS.narrower, URIRef, False),
-    ("related", SKOS.related, URIRef, False),
+    "broader": (SKOS.broader, URIRef, False),
+    "narrower": (SKOS.narrower, URIRef, False),
+    "related": (SKOS.related, URIRef, False),
 
     # SKOS Lexical Labels
-    ("prefLabel", SKOS.prefLabel, Literal, True),
-    ("altLabel", SKOS.altLabel, Literal, True),
-    ("hiddenLabel", SKOS.hiddenLabel, Literal, True),   
+    "prefLabel": (SKOS.prefLabel, Literal, True),
+    "altLabel": (SKOS.altLabel, Literal, True),
+    "hiddenLabel": (SKOS.hiddenLabel, Literal, True),   
     
     # SKOS Notations
-    ("notation", SKOS.notation, Literal, False),
+    "notation": (SKOS.notation, Literal, False),
 
     # SKOS Documentation Properties
-    ("note", SKOS.note, Literal, True),
-    ("changeNote", SKOS.changeNote, Literal, True),
-    ("definition", SKOS.definition, Literal, True),
-    ("editorialNote", SKOS.editorialNote, Literal, True),
-    ("example", SKOS.example, Literal, True),
-    ("historyNote", SKOS.historyNote, Literal, True),
-    ("scopeNote", SKOS.scopeNote, Literal, True),
+    "note": (SKOS.note, Literal, True),
+    "changeNote": (SKOS.changeNote, Literal, True),
+    "definition": (SKOS.definition, Literal, True),
+    "editorialNote": (SKOS.editorialNote, Literal, True),
+    "example": (SKOS.example, Literal, True),
+    "historyNote": (SKOS.historyNote, Literal, True),
+    "scopeNote": (SKOS.scopeNote, Literal, True),
 
     # DCTERMS Metadata Properties
-    ("creator", DCTERMS.creator, Literal, False),
-    ("contributor", DCTERMS.contributor, Literal, False),
-    ("publisher", DCTERMS.publisher, Literal, False),
-    ("rights", DCTERMS.rights, Literal, False),
-    ("source", DCTERMS.source, Literal, False),
-    ("subject", DCTERMS.subject, Literal, True),
-    ("created", DCTERMS.created, Literal, False),
-    ("license", DCTERMS.license, Literal, False),
-    ("modified", DCTERMS.modified, Literal, False),
-    ("title", DCTERMS.title, Literal, True),
-    ("description", DCTERMS.description, Literal, True),
+    "creator": (DCTERMS.creator, Literal, False),
+    "contributor": (DCTERMS.contributor, Literal, False),
+    "publisher": (DCTERMS.publisher, Literal, False),
+    "rights": (DCTERMS.rights, Literal, False),
+    "source": (DCTERMS.source, Literal, False),
+    "subject": (DCTERMS.subject, Literal, True),
+    "created": (DCTERMS.created, Literal, False),
+    "license": (DCTERMS.license, Literal, False),
+    "modified": (DCTERMS.modified, Literal, False),
+    "title": (DCTERMS.title, Literal, True),
+    "description": (DCTERMS.description, Literal, True),
     
     # Other Properties
-    ("seeAlso", RDFS.seeAlso, Literal, False),
-]
+    "seeAlso": (RDFS.seeAlso, Literal, False),
+}
 
 seperator = "|"
 
